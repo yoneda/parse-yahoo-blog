aa0a699041b0b5c418fdf40f0a4999444aa0412d
KoheiYoneda <nsshowdream@gmail.com>
2017-11-05 05:29:39 +0900

    make it possible to collect 200 account

--- collect_account.py
+++ collect_account.py
@@ -2,25 +2,34 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from simplemysql import SimpleMysql
 
 def main():
-    url = "https://blogs.yahoo.co.jp/FRONT/randomblog.html" # yahooブログをランダムに1個取得するURL
-    r = requests.get(url) # requestsはhttpのためのライブラリ
-    redirected_url = r.url
-    print(redirected_url)
+    manCount = 0
+    womenCount = 0
+    db = SimpleMysql(host="127.0.0.1",db="ybdb",user="yoneda",passwd="qweewqq")
+    while True:
+        url = "https://blogs.yahoo.co.jp/FRONT/randomblog.html" # yahooブログをランダムに1個取得するURL
+        r = requests.get(url)
+        redirected_url = r.url
+        account = re.sub(r"(https://blogs.yahoo.co.jp)/(\w+)/(\d+.html)",r"\2",redirected_url)
+        text = r.text
+        soup = BeautifulSoup(text,"html5lib") # html5libはブラウザと同じようにパースしてくれる優れたオプション
+        genderTag = soup.find(class_="usercard__desc")
+        genderText = genderTag.string
+        if genderText.find(u"男性")!=-1 and manCount<100:
+            manCount = manCount + 1
+            db.insert("account",{"name":account,"gender":0})
+            print("account="+account+",gender=0")
+        elif genderText.find(u"女性")!=-1 and womenCount<100:
+            womenCount = womenCount + 1
+            db.insert("account",{"name":account,"gender":1})
+            print("account="+account+",gender=1")
+        if manCount>=100 and womenCount>=100:
+            break
+    db.commit()
+    print("the end")
 
-    account = re.sub(r"(https://blogs.yahoo.co.jp)/(\w+)/(\d+.html)",r"\2",redirected_url)
-    print(account)
-
-    text = r.text
-    soup = BeautifulSoup(text,"html5lib") # html5libはブラウザと同じようにパースしてくれる優れたオプション
-    genderTag = soup.find(class_="usercard__desc")
-    genderText = genderTag.string
-    gender = -1
-    # findメソッドは、ターゲットの文字列が見つかればそのインデックス開始位置を返す。見つからなければ-1を返す。
-    if genderText.find("男性")!=-1: gender = 0
-    elif genderText.find("女性")!=-1: gender = 1
-    print(gender)
 
 if __name__ == "__main__":
     main()
