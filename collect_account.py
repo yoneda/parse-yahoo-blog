32b4324d80ea89f2508ce4bc1024638997f0ea02
KoheiYoneda <nsshowdream@gmail.com>
2017-11-05 03:18:19 +0900

    use regex to extract accountname from url

--- collect_account.py
+++ collect_account.py
@@ -1,21 +1,26 @@
 #coding:utf-8
+import re
 import requests
 from bs4 import BeautifulSoup
 
 def main():
-    # httpライブラリを使ってhtmlを取得
-    url = "https://blogs.yahoo.co.jp/manodonna/15937996.html"
-    r = requests.get(url)
+    url = "https://blogs.yahoo.co.jp/FRONT/randomblog.html" # yahooブログをランダムに1個取得するURL
+    r = requests.get(url) # requestsはhttpのためのライブラリ
+    redirected_url = r.url
+    print(redirected_url)
+
+    account = re.sub(r"(https://blogs.yahoo.co.jp)/(\w+)/(\d+.html)",r"\2",redirected_url)
+    print(account)
+
     text = r.text
-    # html5libでは、ウェブブラウザがしているのと同じようにパースしてくれる。
-    # 他にもhtml.parserやlxmlなどのオプションがある
-    soup = BeautifulSoup(text,"html5lib")
+    soup = BeautifulSoup(text,"html5lib") # html5libはブラウザと同じようにパースしてくれる優れたオプション
     genderTag = soup.find(class_="usercard__desc")
     genderText = genderTag.string
-    # tag = "<div>aaa</div>"
-    # tag.string
-    # =>aaaa
-    print(genderText)
+    gender = -1
+    # findメソッドは、ターゲットの文字列が見つかればそのインデックス開始位置を返す。見つからなければ-1を返す。
+    if genderText.find("男性")!=-1: gender = 0
+    elif genderText.find("女性")!=-1: gender = 1
+    print(gender)
 
 if __name__ == "__main__":
     main()
