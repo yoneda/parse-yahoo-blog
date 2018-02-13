503819030594bd44b0db78f454ff323813772907
shou <nsshowdream@gmail.com>
2018-02-13 16:54:22 +0900

    add extract_gender() to get user's gender

--- scrapping.py
+++ scrapping.py
@@ -16,10 +16,30 @@ def random_account():
     account = re.sub(r"(https://blogs.yahoo.co.jp)/(\w+)/(\d+.html)",r"\2",redirected_url)
     return account
 
+def extract_gender(account):
+    """
+    Yahooブログのあるユーザの性別を取得する
+    @param <str> account アカウント名
+    @return <int> 0であれば男性、1であれば女性、-1であればnotfound
+    """
+    url = "https://blogs.yahoo.co.jp/{}".format(account)
+    r = requests.get(url)
+    text = r.text
+    soup = BeautifulSoup(text,"html5lib")
+    genderTag = soup.find(class_="usercard__desc")
+    genderText = genderTag.string
+    gender = -1
+    if genderText.find(u"男性")!=-1:
+        gender = 0
+    elif genderText.find(u"女性")!=-1:
+        gender = 1
+    return gender
 
 def main():
-    account = random_account()
-    print(account)
+    for i in range(0,30):
+        account = random_account()
+        gender = extract_gender(account)
+        print(gender)
 
 if __name__ == "__main__":
     main()
