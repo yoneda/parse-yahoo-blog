302c3d57340e71b001903a28d31e48a7a4e21e02
shou <nsshowdream@gmail.com>
2018-02-13 19:15:09 +0900

    implement extract_text()

--- scrapping.py
+++ scrapping.py
@@ -1,7 +1,31 @@
 # coding:utf-8
 import re
+import sys
 import requests
 from bs4 import BeautifulSoup
+
+def goodcut_text(text,num):
+    """
+    テキストをきりの良い句読点で区切る関数
+    @param text テキスト
+    @param num きりのいい数字、この数字を超えた直後の最小のインデックスで切られる
+    @return text カットされたテキスト
+    """
+    # 正規表現で「。」を探す
+    pattern = r"。"
+    iterator = re.finditer(pattern,text)
+    starts = []
+    for match in iterator:
+        starts.append(match.start())
+
+    specificIndex = -1
+    for start in starts:
+        if start>num:
+            specificIndex = start
+
+    cuttedText = text[0:specificIndex+1]
+    return(cuttedText)
+
 
 def random_account():
     """
@@ -33,13 +57,59 @@ def extract_gender(account):
         gender = 0
     elif genderText.find(u"女性")!=-1:
         gender = 1
+    else:
+        raise Exception("failed to find gender")
     return gender
 
+def extract_article(account,num):
+    """
+    Yahooブログのあるユーザの記事テキストを取得する
+    @param <str> account アカウント名
+    @param <int> num テキスト文字数
+    @return <str> テキスト
+    """
+    page = 0
+    text = ""
+    while True:
+        page = page + 1
+        if page>num:
+            # ループしすぎた場合は処理を抜ける。
+            # 集めたい文字数はnum個なので、記事の数がnumより大きいわけないという判断から
+            raise Exception("many loop error")
+        url = url = "https://blogs.yahoo.co.jp/" + account + "/MYBLOG/yblog.html";
+        params = {"p":page}
+        html = requests.get(url,params=params).text
+        soup = BeautifulSoup(html,"html5lib")
+        contentTags = soup.findAll(class_ = "entryTd")
+        if len(contentTags)==0:
+            # このpageの記事は0件だったので、ループを抜ける。
+            raise Exception("text too short error")
+        for contentTag in contentTags:
+            contentText = contentTag.text
+            contentText = contentText.replace(u"\n","").replace(u"\r","") # 改行文字を削除
+            contentText = contentText.replace(u" ","").replace(u"　","") # シフトを削除
+            text = text + contentText
+            if len(text)>=num:
+                # 抽出したテキストの文字数がnumを超えた時、関数を抜ける。
+                return text
+
+
 def main():
-    for i in range(0,30):
-        account = random_account()
+    account = random_account()
+
+    try:
         gender = extract_gender(account)
         print(gender)
+    except:
+        print("性別抽出に失敗")
+
+    try:
+        text = extract_article(account,5000)
+        print(text)
+    except:
+        print("テキスト抽出に失敗")
+
+
 
 if __name__ == "__main__":
     main()
